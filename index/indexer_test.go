6dde6695abf45a8155fa4ec4c5ec5172dca99826
egotist <1794206813@qq.com>
2023-04-30 10:33:22 +0800

    add unit testcase for func in bptree.go

--- index/indexer_test.go
+++ index/indexer_test.go
@@ -1,3 +1,27 @@
+/*
+ * This code is based on roseduan's original work, which is
+ * licensed under the Apache License, Version 2.0. The original code can be
+ * found at https://github.com/flower-corp/lotusdb/blob/main/bptree/indexer_test.go.
+ *
+ * Copyright 2022 roseduan
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Portions of this code are licensed under the MIT License.
+ * A copy of the License can be obtained at https://opensource.org/licenses/MIT
+ */
 package index
 
 import (
