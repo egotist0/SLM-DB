8a8d621bbf02b28545c726b112f1bb626a2b2b88
egotist <1794206813@qq.com>
2023-05-06 21:41:36 +0800

    add flush part in cf.go: used to update the index, write vlog and delete the used wal.

--- cf.go
+++ cf.go
@@ -5,15 +5,19 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"sort"
 	"storage/flock"
 	"storage/index"
 	"storage/logfile"
+	"storage/logger"
 	"storage/util"
 	"strconv"
 	"strings"
 	"sync"
+	"syscall"
+	"time"
 )
 
 var (
@@ -225,6 +229,146 @@ func (cf *ColumnFamily) openMemtables() error {
 		} else {
 			cf.immuMems = append(cf.immuMems, table)
 		}
+	}
+	return nil
+}
+
+/*
+flush part: used to update the index, write vlog and delete the used wal.
+*/
+
+func (cf *ColumnFamily) waitWritesMemSpace(size uint32) error {
+	cf.mu.Lock()
+	defer cf.mu.Unlock()
+
+	if !cf.activeMem.isFull(size) {
+		return nil
+	}
+
+	timer := time.NewTimer(cf.opts.MemSpaceWaitTimeout)
+	defer timer.Stop()
+	select {
+	case cf.flushChn <- cf.activeMem:
+		cf.immuMems = append(cf.immuMems, cf.activeMem)
+		// open a new activeMem.
+		ioType := logfile.FileIO
+		if cf.opts.WalMMap {
+			ioType = logfile.MMap
+		}
+		memOpts := memOptions{
+			path:       cf.opts.DirPath,
+			fid:        cf.activeMem.logFileId() + 1,
+			fsize:      int64(cf.opts.MemtableSize),
+			ioType:     ioType,
+			memSize:    cf.opts.MemtableSize,
+			bytesFlush: cf.opts.WalBytesFlush,
+		}
+		if table, err := openMemtable(memOpts); err != nil {
+			return err
+		} else {
+			cf.activeMem = table
+		}
+	case <-timer.C:
+		return ErrWaitMemSpaceTimeout
+	}
+	return nil
+}
+
+// listenAndFlush listen the flushChn and update the index through wal, insert/delete
+// value in vlog and delete the wal after completing the index update.
+func (cf *ColumnFamily) listenAndFlush() {
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	for {
+		select {
+		case table := <-cf.flushChn:
+			var nodes []*index.IndexerNode
+			var deletedKeys [][]byte
+
+			iterateTable := func() error {
+				table.Lock()
+				defer table.Unlock()
+
+				iter := table.sklIter
+				// iterate and write data to bptree and value log(if any).
+				for iter.SeekToFirst(); iter.Valid(); iter.Next() {
+					key := iter.Key()
+					node := &index.IndexerNode{Key: key}
+					mv := decodeMemValue(iter.Value())
+
+					// delete invalid keys from indexer.
+					if mv.typ == byte(logfile.TypeDelete) || (mv.expiredAt != 0 && mv.expiredAt <= time.Now().Unix()) {
+						deletedKeys = append(deletedKeys, key)
+					} else {
+						valuePos, esize, err := cf.vlog.Write(&logfile.LogEntry{
+							Key:       key,
+							Value:     mv.value,
+							ExpiredAt: mv.expiredAt,
+						})
+						if err != nil {
+							return err
+						}
+						node.Meta = &index.IndexerMeta{
+							Fid:       valuePos.Fid,
+							Offset:    valuePos.Offset,
+							EntrySize: esize,
+						}
+						nodes = append(nodes, node)
+					}
+				}
+				return nil
+			}
+
+			if err := iterateTable(); err != nil {
+				logger.Errorf("listenAndFlush: handle iterate table err.%+v", err)
+				return
+			}
+			// update the Bptree index.
+			if err := cf.flushUpdateIndex(nodes, deletedKeys); err != nil {
+				logger.Errorf("listenAndFlush: update index err.%+v", err)
+				return
+			}
+			// delete wal after flush to indexer.
+			if err := table.deleteWal(); err != nil {
+				logger.Errorf("listenAndFlush: delete wal log file err.%+v", err)
+			}
+
+			cf.mu.Lock()
+			if len(cf.immuMems) > 1 {
+				cf.immuMems = cf.immuMems[1:]
+			} else {
+				cf.immuMems = cf.immuMems[:0]
+			}
+			cf.mu.Unlock()
+
+		// cf is forced quit.
+		case <-sig:
+		// cf has closed.
+		case <-cf.closedC:
+			return
+		}
+	}
+}
+
+// flushUpdateIndex update the node in in-memory bptree(delete the needed node simultaneously).
+func (cf *ColumnFamily) flushUpdateIndex(insertNodes []*index.IndexerNode, deletedKeys [][]byte) error {
+	cf.flushLock.Lock()
+	defer cf.flushLock.Unlock()
+
+	// must put and delete in batch.
+	writeOpts := index.WriteOptions{SendDiscard: true}
+	if _, err := cf.indexer.PutBatch(insertNodes, writeOpts); err != nil {
+		return err
+	}
+	if len(deletedKeys) > 0 {
+		if err := cf.indexer.DeleteBatch(deletedKeys, writeOpts); err != nil {
+			return err
+		}
+	}
+
+	// must fsync before delete wal.
+	if err := cf.indexer.Sync(); err != nil {
+		return err
 	}
 	return nil
 }
