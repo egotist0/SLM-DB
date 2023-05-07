63e52f0ebf69075a5fdb528bd96b5e5a3e208c71
egotist <1794206813@qq.com>
2023-05-07 18:29:48 +0800

    add db module: provide the db operation interface for users

--- db.go
+++ db.go
@@ -2,6 +2,8 @@ package storage
 
 import (
 	"errors"
+	"os"
+	"storage/util"
 	"sync"
 )
 
@@ -21,21 +23,82 @@ type DB struct {
 }
 
 // Open a new DB instance, actually will just open the default column family.
-// func Open(opt Options) (*DB, error) {
-// 	if !util.PathExist(opt.DBPath) {
-// 		if err := os.MkdirAll(opt.DBPath, os.ModePerm); err != nil {
-// 			return nil, err
-// 		}
-// 	}
+func Open(opt Options) (*DB, error) {
+	if !util.PathExist(opt.DBPath) {
+		if err := os.MkdirAll(opt.DBPath, os.ModePerm); err != nil {
+			return nil, err
+		}
+	}
 
-// 	db := &DB{opts: opt, cfs: make(map[string]*ColumnFamily)}
-// 	// load default column family.
-// 	if opt.CFOpts.CFName==""{
-// 		opt.CFOpts.CFName==DefaultColumnFamilyName
-// 	}
-// 	if _,err:=db.
-// }
+	db := &DB{opts: opt, cfs: make(map[string]*ColumnFamily)}
+	// load default column family.
+	if opt.CFOpts.CFName == "" {
+		opt.CFOpts.CFName = DefaultColumnFamilyName
+	}
+	if _, err := db.OpenColumnFamily(opt.CFOpts); err != nil {
+		return nil, err
+	}
+	return db, nil
+}
 
+// Put put to default column family.
+func (db *DB) Put(key, value []byte) error {
+	return db.PutWithOptions(key, value, nil)
+}
+
+// PutWithOptions put to default column family with options.
+func (db *DB) PutWithOptions(key, value []byte, opt *WriteOptions) error {
+	columnFamily := db.getColumnFamily(DefaultColumnFamilyName)
+	if columnFamily == nil {
+		return ErrDefaultCfNil
+	}
+	return columnFamily.PutWithOptions(key, value, opt)
+}
+
+// Get get from default column family.
+func (db *DB) Get(key []byte) ([]byte, error) {
+	columnFamily := db.getColumnFamily(DefaultColumnFamilyName)
+	if columnFamily == nil {
+		return nil, ErrDefaultCfNil
+	}
+	return columnFamily.Get(key)
+}
+
+// Delete delete from default column family.
+func (db *DB) Delete(key []byte) error {
+	return db.DeleteWithOptions(key, nil)
+}
+
+// DeleteWithOptions delete from default column family with options.
+func (db *DB) DeleteWithOptions(key []byte, opt *WriteOptions) error {
+	columnFamily := db.getColumnFamily(DefaultColumnFamilyName)
+	if columnFamily == nil {
+		return ErrDefaultCfNil
+	}
+	return columnFamily.DeleteWithOptions(key, opt)
+}
+
+// Sync syncs the content of the column families to disk.
+func (db *DB) Sync() error {
+	for _, cf := range db.cfs {
+		if err := cf.Sync(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// Close close database.
+func (db *DB) close() error {
+	for _, cf := range db.cfs {
+		if err := cf.Close(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// getColumnFamily return the ColumnFamily from db's cfs according to cfName.
 func (db *DB) getColumnFamily(cfName string) *ColumnFamily {
 	db.mu.Lock()
 	defer db.mu.Unlock()
